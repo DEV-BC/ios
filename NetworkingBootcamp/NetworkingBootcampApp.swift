a54312b4b38ee54cd25eb167e29e072b0d702953
Brandon Carr <carrbrandon1716@gmail.com>
2024-08-21 11:21:00 -0500

    Initial Commit

---
+++ NetworkingBootcamp/NetworkingBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkingBootcampApp.swift
+//  NetworkingBootcamp
+//
+//  Created by Brandon Carr on 8/21/24.
+//
+
+import SwiftUI
+
+@main
+struct NetworkingBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
